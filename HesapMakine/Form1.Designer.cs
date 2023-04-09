0b4248661609ae34d877918c2a8ceab9ec72491c
Blu3Line <celal.erkuss@gmail.com>
2023-04-09 16:48:30 +0300

    buton layout ve butonlar ayarlandı

--- HesapMakine/Form1.Designer.cs
+++ HesapMakine/Form1.Designer.cs
@@ -28,20 +28,354 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.btn_bol = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_carp = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_cikar = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_topla = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.btn_temizle = new System.Windows.Forms.Button();
+            this.btn_esittir = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(60)))), ((int)(((byte)(64)))));
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn_bol, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn_7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn_8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn_9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn_carp, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn_4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btn_5, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btn_6, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btn_cikar, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btn_1, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btn_2, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btn_3, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btn_topla, 3, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btn_sil, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.btn_0, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.btn_temizle, 2, 5);
+            this.tableLayoutPanel1.Controls.Add(this.btn_esittir, 3, 5);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 222);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(414, 489);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 75);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(106, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 75);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(209, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 75);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Location = new System.Drawing.Point(312, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 75);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Location = new System.Drawing.Point(3, 84);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 75);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Location = new System.Drawing.Point(106, 84);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(97, 75);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Location = new System.Drawing.Point(209, 84);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(97, 75);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // btn_bol
+            // 
+            this.btn_bol.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_bol.Location = new System.Drawing.Point(312, 84);
+            this.btn_bol.Name = "btn_bol";
+            this.btn_bol.Size = new System.Drawing.Size(99, 75);
+            this.btn_bol.TabIndex = 7;
+            this.btn_bol.Text = "/";
+            this.btn_bol.UseVisualStyleBackColor = true;
+            // 
+            // btn_7
+            // 
+            this.btn_7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_7.Location = new System.Drawing.Point(3, 165);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(97, 75);
+            this.btn_7.TabIndex = 8;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = true;
+            // 
+            // btn_8
+            // 
+            this.btn_8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_8.Location = new System.Drawing.Point(106, 165);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(97, 75);
+            this.btn_8.TabIndex = 9;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = true;
+            // 
+            // btn_9
+            // 
+            this.btn_9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_9.Location = new System.Drawing.Point(209, 165);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(97, 75);
+            this.btn_9.TabIndex = 10;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = true;
+            // 
+            // btn_carp
+            // 
+            this.btn_carp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_carp.Location = new System.Drawing.Point(312, 165);
+            this.btn_carp.Name = "btn_carp";
+            this.btn_carp.Size = new System.Drawing.Size(99, 75);
+            this.btn_carp.TabIndex = 11;
+            this.btn_carp.Text = "x";
+            this.btn_carp.UseVisualStyleBackColor = true;
+            // 
+            // btn_4
+            // 
+            this.btn_4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_4.Location = new System.Drawing.Point(3, 246);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(97, 75);
+            this.btn_4.TabIndex = 12;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            // 
+            // btn_5
+            // 
+            this.btn_5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_5.Location = new System.Drawing.Point(106, 246);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(97, 75);
+            this.btn_5.TabIndex = 13;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            // 
+            // btn_6
+            // 
+            this.btn_6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_6.Location = new System.Drawing.Point(209, 246);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(97, 75);
+            this.btn_6.TabIndex = 14;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = true;
+            // 
+            // btn_cikar
+            // 
+            this.btn_cikar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_cikar.Location = new System.Drawing.Point(312, 246);
+            this.btn_cikar.Name = "btn_cikar";
+            this.btn_cikar.Size = new System.Drawing.Size(99, 75);
+            this.btn_cikar.TabIndex = 15;
+            this.btn_cikar.Text = "-";
+            this.btn_cikar.UseVisualStyleBackColor = true;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_1.Location = new System.Drawing.Point(3, 327);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(97, 75);
+            this.btn_1.TabIndex = 16;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            // 
+            // btn_2
+            // 
+            this.btn_2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_2.Location = new System.Drawing.Point(106, 327);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(97, 75);
+            this.btn_2.TabIndex = 17;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            // 
+            // btn_3
+            // 
+            this.btn_3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_3.Location = new System.Drawing.Point(209, 327);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(97, 75);
+            this.btn_3.TabIndex = 18;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = true;
+            // 
+            // btn_topla
+            // 
+            this.btn_topla.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_topla.Location = new System.Drawing.Point(312, 327);
+            this.btn_topla.Name = "btn_topla";
+            this.btn_topla.Size = new System.Drawing.Size(99, 75);
+            this.btn_topla.TabIndex = 19;
+            this.btn_topla.Text = "+";
+            this.btn_topla.UseVisualStyleBackColor = true;
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_sil.Location = new System.Drawing.Point(3, 408);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(97, 78);
+            this.btn_sil.TabIndex = 20;
+            this.btn_sil.Text = "<=";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            // 
+            // btn_0
+            // 
+            this.btn_0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_0.Location = new System.Drawing.Point(106, 408);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(97, 78);
+            this.btn_0.TabIndex = 21;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = true;
+            // 
+            // btn_temizle
+            // 
+            this.btn_temizle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_temizle.Location = new System.Drawing.Point(209, 408);
+            this.btn_temizle.Name = "btn_temizle";
+            this.btn_temizle.Size = new System.Drawing.Size(97, 78);
+            this.btn_temizle.TabIndex = 22;
+            this.btn_temizle.Text = "CLR";
+            this.btn_temizle.UseVisualStyleBackColor = true;
+            // 
+            // btn_esittir
+            // 
+            this.btn_esittir.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_esittir.Location = new System.Drawing.Point(312, 408);
+            this.btn_esittir.Name = "btn_esittir";
+            this.btn_esittir.Size = new System.Drawing.Size(99, 78);
+            this.btn_esittir.TabIndex = 23;
+            this.btn_esittir.Text = "=";
+            this.btn_esittir.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 658);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(34)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(414, 711);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Hesap Makinesi";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button btn_bol;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_carp;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_cikar;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_topla;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button btn_temizle;
+        private System.Windows.Forms.Button btn_esittir;
     }
 }
 
--- HesapMakine/Form1.cs
+++ HesapMakine/Form1.cs
@@ -15,6 +15,28 @@ namespace HesapMakine
         public Form1()
         {
             InitializeComponent();
+
+            foreach (Control control in tableLayoutPanel1.Controls)//layouttaki kontrolleri geziyor
+            {
+                if (control is Button) // Eğer kontrol bir buton ise
+                {
+                    Button button = (Button)control; // Buton nesnesi olarak tanımla
+                    button.BackColor = tableLayoutPanel1.BackColor; // Butonun arka plan rengini TableLayoutPanel ile aynı renk yap
+                    button.FlatStyle = FlatStyle.Flat;//saçma buton çizgisi vardı onu engelliyor
+                    button.ForeColor = Color.White;
+                }
+            }
         }
+
+        private void Form1_Resize(object sender, EventArgs e)//form resizeable olayını yapmaya çalıştım ama bozuk
+        {
+            this.ClientSize = new Size(430, 750);//TODO: burası hardcoded sonra dinamik olsun 
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
